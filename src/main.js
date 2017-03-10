048c8211dd79f35e89df839717b363f3cb1afc4c
Mr Chen <236514977@qq.com>
2017-03-10 09:41:17 +0800

    历史依赖版本

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import createStore from './store/createStore'
 import { browserHistory, Router } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 import AppContainer from './containers/AppContainer'
 import 'antd/dist/antd.css';
 
